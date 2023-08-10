2bd2a21d13ab4cbe1d7cdfcb855e44f5c09d3c1c
Tyrel <tyrel.hiebert@gmail.com>
2023-08-10 13:22:38 -0700

    refactor hardcoded values to args

--- generateEmails.py
+++ generateEmails.py
@@ -1,24 +1,51 @@
 import random
-import sys
+import argparse
 
-emailPrefix = "matt"
-emailDomain = "giftbit.com"
-numberOfEmails = 2000
-
-# TODO: add args flag to output json or not
+def validateArgs():
+    parser = argparse.ArgumentParser(description="Quotify List")
+    parser.add_argument(
+        "--email",
+        required=True,
+        type=str,
+        help="The prefix on the email. For example tyrel@giftbit.com, tyrel is the prefix.",
+    )
+    parser.add_argument(
+        "--domain",
+        required=True,
+        type=str,
+        help="Domain for the email address",
+    )
+    parser.add_argument(
+        "--amount",
+        required=False,
+        type=int,
+        help="Number of emails to generate. Default is 10 if amount excluded.",
+    )
+    parser.add_argument(
+        "--newline",
+        required=False,
+        action="store_true",
+        help='Delimiter you want between quoted line items. For example "\n"'
+    )
+    global args
+    args = parser.parse_args()
+    
+    if not args.amount or args.amount < 0:
+        args.amount = 10
 
 def main():
-    filename = "output_"+ str(numberOfEmails) + "_" + str(random.randint(0, 999)) + ".csv"
+    filename = "email_output_"+ str(args.amount) + "_" + str(random.randint(0, 999)) + ".csv"
     outputFile = open(filename, 'w')
 
     outputFile.write('"contacts": [' + "\n")
 
-    for n in range(0, numberOfEmails):
-        outputFile.write(f'  {{ "email": "{emailPrefix}+{str(n)}@{emailDomain}" }},' + "\n")
+    for n in range(0, args.amount):
+        outputFile.write(f'  {{ "email": "{args.email}+{str(n)}@{args.domain}" }},' + "\n")
     
     outputFile.write(']' + "\n")
     
     print(filename + " generated.")
 
 if __name__ == '__main__':
+    validateArgs()
     main()
